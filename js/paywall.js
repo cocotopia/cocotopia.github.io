4baf98872a80190929e0ce2c33d4caa9084a60c7
Jorge Duenas <cocotopia@gmail.com>
2019-07-09 19:12:21 -0500

    update paywall

--- js/paywall.js
+++ js/paywall.js
@@ -5557,7 +5557,7 @@ function getHostnameFromURL(url) {
         },
         customRegCheck: function () {
             var start = Date.now();
-            var isLoggedIn = window.Identity._userIdentity.accessToken ? true : false;
+            var isLoggedIn = window.localStorage.getItem('ArcId.USER_PROFILE') ? true : false;
             return Promise.resolve({
                 l: isLoggedIn,
                 timeTaken: Date.now() - start
